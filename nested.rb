cb4e1d75f72cc17249b105051b5a45d86c57f056
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-05 23:26:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -114,6 +114,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:alan_kay][:known_for].push("Assembly")
+    programmer_hash[:alan_kay][:known_for] << "Assembly"
     programmer_hash
 end
